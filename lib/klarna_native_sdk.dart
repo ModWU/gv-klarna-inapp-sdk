c06f015974e39b9d5a9df43c5fad72f065573622
ModWU <985477819@qq.com>
2021-09-06 18:59:51 +0800

    add more interface

--- lib/klarna_native_sdk.dart
+++ lib/klarna_native_sdk.dart
@@ -12,9 +12,28 @@ class KlarnaNativeSDK {
   static const MethodChannel _channel =
   const MethodChannel('klarna_native_sdk');
 
+  /// return [authToken]
   static Future<String> authorizingSession() async {
     return await _channel
         .invokeMethod('authorizingSession');
+  }
+
+  /// return [authToken]
+  static Future<String> reauthorize() async {
+    return await _channel
+        .invokeMethod('reauthorize');
+  }
+
+  /// return [authToken]
+  static Future<String> finalize() async {
+    return await _channel
+        .invokeMethod('finalize');
+  }
+
+  /// return [showForm]
+  static Future<bool> loadPaymentReview() async {
+    return await _channel
+        .invokeMethod('loadPaymentReview');
   }
 
   static void setListener(CallbackListener listener) async {
